f0d15b102030bf4f4f87df21ef6a108c09ee2fe6
Koutarou Chikuba <miz404@gmail.com>
2012-03-04 04:44:48 +0900

    delete files

--- inits/00-plugins.el
+++
@@ -1,9 +0,0 @@
-; auto-complete
-(require 'auto-complete)
-(global-auto-complete-mode t)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/dict")
-(require 'auto-complete-config)
-(ac-config-default)
-
-; elscreen
-(load  "elscreen" "ElScreen" t)
