62368dc4b0a7d30755e2ae36df57026b1d7d059e
Koutarou Chikuba <miz404@gmail.com>
2014-02-06 02:12:39 +0900

    Fix fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,9 +1,8 @@
 set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/homebrew/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
 rbenv rehash >/dev/null ^&1
 
-# OPAM configuration
-# . /Users/mizchi/.opam/opam-init/init.fish > /dev/null 2> /dev/null || true
 alias b="bundle"
 alias be="bundle exec"
 alias ber="bundle exec rake"
